19cc3fe343442d849815b7a44be371b75f11d194
David Manouchehri <manouchehri@riseup.net>
2017-01-24 00:21:06 -0500

    logging

--- tests/r2dev.py
+++ tests/r2dev.py
@@ -1,9 +1,15 @@
 import cle
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
 classic = cle.Loader("test.elf", main_opts={'backend': 'elf', 'custom_arch': 'i386'})
 
 r2 = cle.Loader("test.elf", main_opts={'backend': 'r2', 'custom_arch': 'i386'})
 
 print classic
+print r2
+
+
+
 
