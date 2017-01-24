2a976216c25350c848808f0ac3b8f067c562a8f7
David Manouchehri <manouchehri@riseup.net>
2017-01-24 02:40:31 -0500

    Fail test.

--- tests/r2dev.py
+++ tests/r2dev.py
@@ -23,3 +23,12 @@ print project
 print classic
 print r2
 
+path_group = project.factory.path_group()
+
+FIND_ADDR = 0x08048533 # mov dword [esp], str.Congrats_ ; [0x8048654:4]=0x676e6f43 LEA str.Congrats_ ; "Congrats!" @ 0x8048654
+AVOID_ADDR = 0x08048554 # mov dword [esp], str.Wrong_ ; [0x804865e:4]=0x6e6f7257 LEA str.Wrong_ ; "Wrong!" @ 0x804865e
+
+
+path_group.explore(find=FIND_ADDR, avoid=AVOID_ADDR)
+
+print path_group.found[0].state.posix.dumps(0).split('\0')[0]
